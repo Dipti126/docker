042f27d9c2b6b4ff5bd3ab49bf0db9b1633d6511
Dipti Vashi <diptivashi@gmail.com>
2016-03-12 16:28:20 -0800

    new edited

--- contributing.md
+++ contributing.md
@@ -1,1 +1,7 @@
 Here is my file to contributing my project.
+print a
+print a\
+print a
+print a
+print a
+print a
---
+++ first_project.py
@@ -0,0 +1,11 @@
+import os
+
+
+print "Hi my name is Dipti"
+print "Hi my name is Dipti"
+print "Hi my name is Dipti"
+print "Hi my name is Dipti"
+print "Hi my name is Dipti"
+print "Hi my name is Dipti"
+a= ["1111","11111111","eywgrekwugr","geuywirtrgegr"]
+print a
